0ce2926d1b117ef6baeaf12fa7669ce9df59346e
DebelToni <hristov.anton@icloud.com>
2025-05-24 16:43:46 +0300

    Back for good storage + rng bet batch

--- v1/model/Training_step.py
+++ v1/model/Training_step.py
@@ -31,13 +31,25 @@ def loss_fn(params, batch):
 # @jax.jit(static_argnames=['model', 'optimizer'])
 # def train_step(params, opt_state, batch, *, model, optimizer):
 @partial(jax.jit,static_argnames=['model', 'optimizer'])
-def train_step(params, opt_state, batch, *, model, optimizer):
-
+# def train_step(params, opt_state, batch, *, model, optimizer):
+#
+#     def loss_fn(p):
+#         logits  = model.apply({"params": p}, batch["input"])
+#         loss    = optax.softmax_cross_entropy_with_integer_labels(
+#                       logits, batch["target"])
+#         loss    = (loss * batch["mask"]).sum() / batch["mask"].sum()
+#         return loss
+#
+#     (loss, grads) = jax.value_and_grad(loss_fn)(params)
+#     updates, opt_state = optimizer.update(grads, opt_state, params)
+#     new_params = optax.apply_updates(params, updates)
+#     return new_params, opt_state, loss
+def train_step(params, opt_state, batch, *, model, optimizer, dropout_rng):
     def loss_fn(p):
-        logits  = model.apply({"params": p}, batch["input"])
-        loss    = optax.softmax_cross_entropy_with_integer_labels(
-                      logits, batch["target"])
-        loss    = (loss * batch["mask"]).sum() / batch["mask"].sum()
+        logits = model.apply({"params": p}, batch["input"], rngs={"dropout": dropout_rng})
+        loss = optax.softmax_cross_entropy_with_integer_labels(
+            logits, batch["target"])
+        loss = (loss * batch["mask"]).sum() / batch["mask"].sum()
         return loss
 
     (loss, grads) = jax.value_and_grad(loss_fn)(params)

12d1acc16239d5f5088af5a91c5a6185948c3909
DebelToni <hristov.anton@icloud.com>
2025-05-26 22:09:57 +0300

    dsadsa

--- v1/setup.sh
+++ v1/setup.sh
@@ -1,7 +1,7 @@
 git pull
 apt update
 apt install -y vim
-pythom -m pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -r ../requirements.txt
 pip install --upgrade jax[cuda12] jaxlib flax
 pip install flash-attn-jax

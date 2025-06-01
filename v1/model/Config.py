8d4e3ecf08ede2c99cbcb66e3479c905f8f5bc08
DebelToni <anton.ivanov.hristov@gmail.com>
2025-06-01 13:46:15 +0300

    Feed forward splititng made dynamic + 4*

--- v1/model/Config.py
+++ v1/model/Config.py
@@ -10,11 +10,11 @@ param_dtype = jnp.float32
 # -----------------------------
 
 # Model Hyperparameters
-embedding_size = 256
-context_length = 257
-num_heads = 2
-num_layers = 2
-feed_forward_size = num_layers * embedding_size
+embedding_size = 256 # 768
+context_length = 257 # 1024
+num_heads = 2 # 12
+num_layers = 2 # 12
+feed_forward_size = 4 * embedding_size # 4 is standard in transformer models
 tokenizer_name = "EleutherAI/gpt-neo-125M"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 dropout_rate = 0.1
@@ -22,11 +22,11 @@ dropout_rate = 0.1
 vocab_size = tokenizer.vocab_size
 
 # Training Hyperparameters
-learning_rate = 2e-4
+learning_rate = 2e-4 # 1e-4
 weight_decay = 1e-2
-batch_size = 8
-num_epochs = 1
-acc_steps = 2
+batch_size = 8 # 16
+num_epochs = 1 # 5
+acc_steps = 2 # 4
 
 # Other Settings
 use_remat = True

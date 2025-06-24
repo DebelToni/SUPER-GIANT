d7120ba1046f12d13768e6522063a6853d99b2fb
DebelToni <anton.ivanov.hristov@gmail.com>
2025-06-24 12:49:12 +0300

    fixed the transformer

--- v1/model/Transformer_block.py
+++ v1/model/Transformer_block.py
@@ -484,7 +484,4 @@ class TinyTransformerBlock(nn.Module):
             h_ffn = nn.Dropout(rate=module.dropout_rate)(h_ffn, deterministic=deterministic)
             return residual + h_ffn # bf16
 
-        # return _block(self, x)
-        goshko toa fsaklfjalksd fadslk j
-        return "THIS IS WRONG"
-        return "THIS IS WRONG"
+        return _block(self, x)

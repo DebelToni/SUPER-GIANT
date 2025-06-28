de1ee64ca33f7102faa0a519627877da79cce8d3
DebelToni <anton.ivanov.hristov@gmail.com>
2025-06-28 11:12:45 +0300

    Transformer cleaner

--- v1/model/Transformer_block.py
+++ v1/model/Transformer_block.py
@@ -484,4 +484,4 @@ class TinyTransformerBlock(nn.Module):
             h_ffn = nn.Dropout(rate=module.dropout_rate)(h_ffn, deterministic=deterministic)
             return residual + h_ffn # bf16
 
-    retunr bandaslhdsalkj
+        return _block(self, x)

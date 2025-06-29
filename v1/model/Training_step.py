f4b932b7ba1c0543163665f19df9804c7e59a1c4
DebelToni <anton.ivanov.hristov@gmail.com>
2025-06-29 20:36:04 +0300

    giant vocab size and rng in training step adjustopment

--- v1/model/Training_step.py
+++ v1/model/Training_step.py
@@ -7,7 +7,7 @@ def train_step(params, opt_state, batch, *, model, optimizer, dropout_rng):
         logits = model.apply(
             {"params": p},
             batch["input"],
-            rngs={"dropout": dropout_rng},
+            rng=dropout_rng,
             deterministic=False,
         )
         loss = optax.softmax_cross_entropy_with_integer_labels(
@@ -15,7 +15,7 @@ def train_step(params, opt_state, batch, *, model, optimizer, dropout_rng):
         loss = (loss * batch["mask"]).sum() / batch["mask"].sum()
         return loss
 
-    (loss, grads) = jax.value_and_grad(loss_fn)(params, rng=dropout_rng)
+    (loss, grads) = jax.value_and_grad(loss_fn)(params)
     updates, opt_state = optimizer.update(grads, opt_state, params)
     new_params = optax.apply_updates(params, updates)
     return new_params, opt_state, loss

832ce3b91516481a4fd4da2a5d0ac0e86abf817c
DebelToni <anton.ivanov.hristov@gmail.com>
2025-07-29 00:17:41 +0200

    len tokenizer for acutarte tokenizer size

--- v1/model/Run_training.py
+++ v1/model/Run_training.py
@@ -77,7 +77,7 @@ def main() -> None:
     # Model
     # ------------------------------------------------------------------ #
     model = GiantGPT(
-        vocab_size     = tokenizer.vocab_size,
+        vocab_size     = len(tokenizer),
         context_length = Config.context_length,
         d_model        = Config.embedding_size,
         n_heads        = Config.num_heads,
